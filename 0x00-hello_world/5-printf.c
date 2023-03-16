33d164a459ef43fa7a8788eceda9a2ae41748de3
stormzee <soppongaddo@gmail.com>
2023-03-16 09:23:38 -0700

    print string using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Prints with proper grammar, but the outcome is a piece of art,
- * Return: Success (0), Always 
+ * Return: Success (0), Always
  */
 int main(void)
 {
