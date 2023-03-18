1a2b8cd43fd717420f3499c8f5690522b7e14e8b
stormzee <soppongaddo@gmail.com>
2023-03-17 23:33:04 -0700

    check if last digit of number is >5, <6, ==0 and !=0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,6 +11,7 @@ int main(void)
 {
 	int n;
 	int last_digit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
@@ -18,8 +19,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n",
 			n, last_digit);
 	else if (last_digit == 0)
-		printf("Last digit of %d is %d and is 0\n",
-			n, last_digit);
+		printf("Last digit of %d is %d and is 0\n",n, last_digit);
 	else if (last_digit < 6 &&  n != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 			n, last_digit);
