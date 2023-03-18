405c14cde27673f22af6c5fc600a18f2932279b9
stormzee <soppongaddo@gmail.com>
2023-03-17 22:58:15 -0700

    print number based on >,< or == 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * main - Prints a number based on a condition.
+/**
+ * main - Prints a number based on the conditions.
+ * if number is greater than 0, positive
+ * if number is less than 0, negative
+ * 0 if number is equal to zero
  * Return: Success (0), Always.
  */
 int main(void)
@@ -11,12 +14,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0){
+	if (n > 0)
 		printf("%d is positive\n", n);
-	}else if (n < 0){
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	}else{
-		printf("%d is zero\n",n);
-	}
+	else
+		printf("%d is zero\n", n);
 	return (0);
 }
