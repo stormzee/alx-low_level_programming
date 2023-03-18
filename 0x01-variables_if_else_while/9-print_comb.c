1fc6bce6e4ee6a19a9fb73029b48b054c7f89399
stormzee <soppongaddo@gmail.com>
2023-03-18 08:44:15 +0000

    print all combinations of single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,6 +8,7 @@ int main(void)
 	int i, j;
 
 	for (i = 0; i <= 10; i++)
+	{
 		for (j = 1; j <= 10; j++)
 		{
 			putchar(i + '0');
@@ -15,5 +16,7 @@ int main(void)
 			putchar(j + '0');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
+	return (0);
 }
