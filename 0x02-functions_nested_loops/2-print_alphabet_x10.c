5a587c143e712598f983ccd174298c8cf5973549
stormzee <soppongaddo@gmail.com>
2023-03-22 12:23:46 +0000

    print a-z 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* print_alphabet_x10.c - print alphabets a-z ten times
+ *
+ * Return: Success(0), Always otherswise exit with error
+ */
+void print_alphabet_x10(void)
+{
+	char c[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(c);
+	return (0);
+}
