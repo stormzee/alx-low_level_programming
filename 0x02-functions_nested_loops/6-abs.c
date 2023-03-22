ee363a659531b44435962f85c06c44d76e23d2ef
stormzee <soppongaddo@gmail.com>
2023-03-22 14:57:45 +0000

    return absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - get the absolute value of a number
+ *
+ * @c: parameter to check for
+ * Return: 1 if parameter is alphabet and 0 otherwise
+ */
+int _abs(int c)
+{
+	if (c < 0)
+		return (-c);
+	else
+		return (c);
+}
