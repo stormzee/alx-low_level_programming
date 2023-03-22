0075b56141fb32ff0b436069bf8b88b89b76edf6
stormzee <soppongaddo@gmail.com>
2023-03-22 17:06:06 +0000

    print the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,20 +16,19 @@ void times_table(void)
 
 			if (product < 10)
 			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(product % 10 + '0');
 			}
 			else
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
 			}
-			if (col < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			} else
-				_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
