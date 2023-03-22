65b4e8bc015f3fe93418af0d25effb386a73ec7a
stormzee <soppongaddo@gmail.com>
2023-03-22 12:40:16 +0000

    print a-z 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,9 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
-	char c[] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char c = 'a';
+	int i, j;
 
-	for (i = 0; i <= 9; i++)
-		_putchar(c + '0');
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(c);
+			c++;
+		}
+		/*set c=a to restart printing a-z*/
+		c = 'a';
+		_putchar('\n');
+	}
 }
