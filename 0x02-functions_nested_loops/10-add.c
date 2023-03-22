34f5d53b1be7e4702163cc0945bffa0642621960
stormzee <soppongaddo@gmail.com>
2023-03-22 17:21:41 +0000

    function to cal sum of two int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  * _isalpha - check whether the input parameter is alphabet
+ *   *
+ * @c: first parameter to add
+ * @b: second parameter to add to the first one
+ * Return: Sum of the two input parameters
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return sum;
+}
