8a67790df0f715ce4dc8fea097777082834d257a
stormzee <soppongaddo@gmail.com>
2023-03-22 16:37:03 +0000

    print the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,12 +13,16 @@ void times_table(void)
 		for (col = 0; col < 10; col++)
 		{
 			product = row * col;
-			/*print the first value of the product using the division */
-			_putchar(product / 10 + '0');
-			/*print the last digit of the product usng the modulo */
-			_putchar(product % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
+
+			if (product < 10)
+				_putchar(product % 10 + '0');
+			else
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
