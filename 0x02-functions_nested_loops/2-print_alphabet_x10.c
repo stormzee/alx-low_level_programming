07e2af3748ec24ee69ad1c9cd91c406fb2cec38a
stormzee <soppongaddo@gmail.com>
2023-03-22 12:42:43 +0000

    print a-z 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10.c - print alphabets a-z ten times
+ * print_alphabet_x10 - print alphabets a-z ten times
  *
  * Return: Success(0), Always otherswise exit with error
  */
