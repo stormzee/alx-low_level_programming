33c8ef69619b31daaddc668214b04fc91c35bb30
stormzee <soppongaddo@gmail.com>
2023-03-22 14:43:19 +0000

    print the sign infornt of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,12 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		return(1);
-	}else if (n == 0)
+		return (1);
+	} else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
-	}else
-	{
-		_putchar('-');
-		return (-1);
 	}
-} 
+	_putchar('-');
+	return (-1);
+}
