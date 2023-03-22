c139c670f78a3746fd02b51e06795b2a3468dc97
stormzee <soppongaddo@gmail.com>
2023-03-22 17:11:40 +0000

    print the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 			product = row * col;
 			if (col == 0)
 				_putchar(product + '0');
-			if (product < 10)
+			if (product < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
