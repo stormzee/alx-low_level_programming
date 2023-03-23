88f40c63fe1b647580740281cd8b84f019b4e362
stormzee <soppongaddo@gmail.com>
2023-03-23 15:34:44 +0000

    program to print numbers from 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,13 @@
 #include "main.h"
 /**
- * print_numbers - Prints the numbers from 0 to 9.
+ *print_numbers - Prints the numbers from 0 to 9.
  *Return: Success (0), Always.
  */
 void print_numbers(void)
 {
-	int i;
+	int c;
 
-	i = 0;
-	while (i <= 9)
-	{
-		_putchar(i % 10 + '0');
-		i++;
-	}
+	for (c =0; c <= 9; c++)
+		_putchar(c % 10 + '0');
 	_putchar('\n');
-	return (0);
 }
