04f8d337613beee34f6cbbbc8ce02665fe96bf07
stormzee <soppongaddo@gmail.com>
2023-03-23 22:14:43 +0000

    print more numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *print_numbers - Prints the numbers from 0 to 9.
- *Return: Success (0), Always.
+ * print_numbers - Prints the numbers from 0 to 9.
+ * Return: Success (0), Always.
  */
 void print_numbers(void)
 {
 	int c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = 0; c < 10; c++)
 	{
 		if (c == 2 || c == 4)
 			continue;
