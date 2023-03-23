6f0fe69bad8d29b1b289f4e3274d76ea38788850
stormzee <soppongaddo@gmail.com>
2023-03-23 14:48:43 +0000

    program to check for digit from 0-9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,8 @@
  */
 int _isdigit(int c)
 {
-	if (c < 0)
-	{
-		c = c * -1;
-
-		if (c >= 0 && c <= 9)
-			return (1);
-	} else
+	if ((c >= 0 && c <= 9) || (c <= -1 && c >= -9))
+		return (1);
+	else
 		return (0);
 }
