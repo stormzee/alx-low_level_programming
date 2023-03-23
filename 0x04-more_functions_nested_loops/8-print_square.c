8dcf61baae02f96b5e238a33d6f16e7f733fc541
stormzee <soppongaddo@gmail.com>
2023-03-23 20:16:11 +0000

    print disquare in the terminal

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - print a square using the hash sign
+ * @size: the size of the square
+ * Return: Success (0), Always.
+ */
+int print_square(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (col = 1; col <= size; col++)
+				_putchhar('#');
+			_putchar('\n');
+		}
+	}
+}
