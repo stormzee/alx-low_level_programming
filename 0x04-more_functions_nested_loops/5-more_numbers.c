3e3cc8dfaf3527b86f50ca5ee6060f3dd92dcee8
stormzee <soppongaddo@gmail.com>
2023-03-23 19:03:59 +0000

    print  from 0-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *more_numbers - prints numbers from 0-14
+ *Return: Success (0), Always
+ */
+void more_numbers(void)
+{
+	int row, col;
+
+	for (row = 0; row <= 10; row++)
+	{
+		for (col = 0; col <= 14; col++)
+		{
+			_putchar(col % 10 + '0');
+			if (col > 9)
+				_putchar(col / 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
