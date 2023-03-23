947f1d7585acce3eb4d96abaedb459469ac8cc09
stormzee <soppongaddo@gmail.com>
2023-03-23 14:38:38 +0000

    program to check for digit from 0-9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
- *_isdigit - checks whether input parameter is digit - 
- *
+ *_isdigit - checks whether input parameter is digit
  * @c: parameter to check for whether it is digit
- *
- *Return: 1 if c is digit and 0 otherwise 
+ *Return: 1 if c is digit and 0 otherwise
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
-		return (1);
-	else
-		return (0);
+	if (c < 0)
+	{
+		c = -c;
+
+		if (c >= 0 && c <= 9)
+			return (1);
+		else
+			return (0);
+	}
 }
