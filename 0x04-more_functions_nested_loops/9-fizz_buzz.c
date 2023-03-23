13787f9e1995fb111151e929b0e2ddcd343b1cd7
stormzee <soppongaddo@gmail.com>
2023-03-23 20:44:49 +0000

    print numbers from 1-100 and put fizz buzz in places where the num is a multiple of 3 or 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - prints from 1 to 100 and put fizz, buzz
+ * or fizzbuzz for multiples of 3, 5 and of both
+ * respectively
+ * Return: Success (0), Always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			if (i == 100)
+				printf("Fizz");
+		} else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			if (i == 100)
+				printf("Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+			if (i == 100)
+				printf("FizzBuzz");
+		} else
+		{
+			printf("%d ", i);
+			if (i == 100)
+				printf("%d", i);
+		}
+	}
+}
