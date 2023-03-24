10a2e5b28cddb87a470f7ca3051694861fc1f4ee
stormzee <soppongaddo@gmail.com>
2023-03-24 16:13:08 +0000

    fix print numbers from 0-9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,13 @@
  */
 void print_numbers(void)
 {
-	int c;
+	int i;
 
-	for (c = 0; c < 10; c++)
+	for (i = 0; i < 10; i++)
 	{
-		if (c == 2 || c == 4)
+		if ((i == 2) || (i == 4))
 			continue;
-		_putchar(c + '0');
+		_putchar((i % 10) + '0');
 	}
 	_putchar('\n');
 }
