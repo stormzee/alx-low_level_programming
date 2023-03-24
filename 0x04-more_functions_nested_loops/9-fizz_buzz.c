ccf79451e745e2161c42cb1c1af6702af4176838
stormzee <soppongaddo@gmail.com>
2023-03-24 14:49:19 +0000

    fix fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 	printf("%d", i);
 	for (i = 2; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf(" FizzBuzz");
 		}
@@ -23,8 +23,6 @@ int main(void)
 		else if (i % 5 == 0)
 		{
 			printf(" Buzz");
-			if (i == 100)
-				printf("Buzz");
 		}
 		else
 		{
