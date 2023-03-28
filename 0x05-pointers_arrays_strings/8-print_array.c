91cdbab2e08fb5e50fafcfc2d432978c7c4c4fd9
stormzee <soppongaddo@gmail.com>
2023-03-28 13:59:24 +0000

    print n number of elements from an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,9 +15,9 @@ void print_array(int *a, int n)
 		printf("%d", a[0]);
 	else
 	{
-		for (i = 0; i <= n-1; i++)
+		for (i = 0; i <= n - 1; i++)
 		{
-			if (i == n-1)
+			if (i == n - 1)
 				printf("%d", a[i]);
 			else
 				printf("%d, ", a[i]);
