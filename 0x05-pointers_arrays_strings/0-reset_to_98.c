86d3afaf5f9f4302d7f37fe51bf1325bfabf5003
stormzee <soppongaddo@gmail.com>
2023-03-28 05:47:18 +0000

    reset num to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * reset_to_98 - sets a number to 98
- * @*n: number to chaange its values to 98
+ * @n: number to chaange its values to 98
  * Return:
  */
 void reset_to_98(int *n)
