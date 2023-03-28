746e9db6bd0b988afc9aeb51afbddfd28057a8b5
stormzee <soppongaddo@gmail.com>
2023-03-28 05:50:03 +0000

    get the length of str

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strlen - get the length if a string
- * @*s: pointer to the character s.
+ * @s: pointer to the character s.
  * Return: the length of a string
  */
 int _strlen(char *s)
