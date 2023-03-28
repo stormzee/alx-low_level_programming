46478ed9e20e8dbfda38e71f249232f30c66f5e8
stormzee <soppongaddo@gmail.com>
2023-03-28 05:37:55 +0000

    prints every other string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - return every other character of the string
+ * @str: string to return every other character
+ * Return: Success (0), Always
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i+=2;
+	}
+	_putchar('\n');
+}
