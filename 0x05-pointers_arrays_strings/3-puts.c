c8d4c1c4f17457a064c091fb3c9ab97e5f470817
stormzee <soppongaddo@gmail.com>
2023-03-28 05:51:41 +0000

    print str

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _puts - prints a string, followed by newline
+ * @str: char param to get the length
  * Return: Success(0), Always
  */
 void _puts(char *str)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
