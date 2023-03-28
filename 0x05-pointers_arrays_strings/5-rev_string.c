9c7b749cc7374a2921edcc27273ec97868564498
stormzee <soppongaddo@gmail.com>
2023-03-28 15:41:50 +0000

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,5 +6,23 @@
  */
 void rev_string(char *s)
 {
+	int i, len, tmp;
+	*p1 = s;
+	*p2 = s;
 
+	len = 0;
+	while (*p2 != '\0')
+	{
+		len++;
+		p2++;
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		/*set tmp to be the first element*/
+		tmp = *(p1 + i);
+		/*set the first element to be the last element using ptr*/
+		*(p1 + i) = *(p2 - i - 1);
+		/*set the last element to the tmp(first element*/
+		*(p2 - i - 1) = tmp;
+	}
 }
